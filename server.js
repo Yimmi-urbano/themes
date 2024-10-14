071cd95e443450bc0edf1fbf6d89ae0939e57820
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-10-13 22:45:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 // Usar las rutas de temas
 app.use('/api', themeRoutes);
 
